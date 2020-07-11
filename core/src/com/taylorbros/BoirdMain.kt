574dfd39946697eacb53539f8634937d41c40c71
Michael Taylor <michael96taylor@gmail.com>
2020-07-11 02:51:35 -0400

    Added some bull that doesn't work

--- core/src/com/taylorbros/BoirdMain.kt
+++ core/src/com/taylorbros/BoirdMain.kt
@@ -4,21 +4,30 @@ import com.badlogic.gdx.ApplicationAdapter
 import com.badlogic.gdx.Gdx
 import com.badlogic.gdx.graphics.GL20
 import com.badlogic.gdx.graphics.Texture
+import com.badlogic.gdx.graphics.g2d.Animation
 import com.badlogic.gdx.graphics.g2d.SpriteBatch
+import com.badlogic.gdx.graphics.g2d.TextureRegion
 
 class BoirdMain : ApplicationAdapter() {
     var batch: SpriteBatch? = null
     var img: Texture? = null
+    var animationFrames: Array<TextureRegion>? = null
+    var animation: Animation<TextureRegion>? = null
+    var elapsedTime: Float = 0f
+
     override fun create() {
         batch = SpriteBatch()
-        img = Texture("badlogic.jpg")
+        img = Texture("birdUp.png")
+        var tmpFrames = TextureRegion.split(img, 8, 8)
+        animationFrames = tmpFrames[0]
+        animation = Animation(.25f, animationFrames!!)
     }
 
     override fun render() {
-        Gdx.gl.glClearColor(1f, 0f, 0f, 1f)
+        elapsedTime += Gdx.graphics.deltaTime
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT)
         batch!!.begin()
-        batch!!.draw(img, 0f, 0f)
+        batch!!.draw(animation!!.getKeyFrame(elapsedTime, true), 0f, 0f)
         batch!!.end()
     }
 
