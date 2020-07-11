6bba73eda920c7ca0327878e4f9f23c0c2a53751
Michael Taylor <michael96taylor@gmail.com>
2020-07-11 02:05:31 -0400

    Converted to Kotlin and added bird assets

---
+++ desktop/src/com/taylorbros/desktop/DesktopLauncher.kt
@@ -0,0 +1,13 @@
+package com.taylorbros.desktop
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration
+import com.taylorbros.BoirdMain
+
+object DesktopLauncher {
+    @JvmStatic
+    fun main(arg: Array<String>) {
+        val config = LwjglApplicationConfiguration()
+        LwjglApplication(BoirdMain(), config)
+    }
+}
