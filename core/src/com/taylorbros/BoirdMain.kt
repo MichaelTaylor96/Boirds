57dbed1ded0ab777cef46990fe7e94f34027475d
Michael Taylor <michael96taylor@gmail.com>
2020-07-11 11:19:35 -0400

    Fixed animation test

--- core/src/com/taylorbros/BoirdMain.kt
+++ core/src/com/taylorbros/BoirdMain.kt
@@ -7,6 +7,7 @@ import com.badlogic.gdx.graphics.Texture
 import com.badlogic.gdx.graphics.g2d.Animation
 import com.badlogic.gdx.graphics.g2d.SpriteBatch
 import com.badlogic.gdx.graphics.g2d.TextureRegion
+import com.badlogic.gdx.utils.Array
 
 class BoirdMain : ApplicationAdapter() {
     var batch: SpriteBatch? = null
@@ -17,17 +18,17 @@ class BoirdMain : ApplicationAdapter() {
 
     override fun create() {
         batch = SpriteBatch()
-        img = Texture("birdUp.png")
-        var tmpFrames = TextureRegion.split(img, 8, 8)
-        animationFrames = tmpFrames[0]
-        animation = Animation(.25f, animationFrames!!)
+        img = Texture("birdEat.png")
+        var tmpFrames = TextureRegion.split(img, 16, 16)
+        animationFrames = Array<TextureRegion>(tmpFrames[0])
+        animation = Animation<TextureRegion>(.1f, animationFrames!!)
     }
 
     override fun render() {
         elapsedTime += Gdx.graphics.deltaTime
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT)
         batch!!.begin()
-        batch!!.draw(animation!!.getKeyFrame(elapsedTime, true), 0f, 0f)
+        batch!!.draw(animation!!.getKeyFrame(elapsedTime, true), 0f, 0f, Gdx.graphics.width.toFloat(), Gdx.graphics.height.toFloat())
         batch!!.end()
     }
 
